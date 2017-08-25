bc4ddce84945f007f6d18ea1ce6cb12d82eb160b
Kush Jain <kush.jain@joshtechnologygroup.com>
2017-08-25 21:02:14 +0530

    Store Query Information

--- engine/dark_matter/query_parser/parser.py
+++ engine/dark_matter/query_parser/parser.py
@@ -1,6 +1,9 @@
 import RAKE
 from nltk import corpus
 
+from dark_matter.query_parser import models as query_models
+from dark_matter.keywords import models as keyword_models
+
 
 class Parser(object):
     """
@@ -9,6 +12,21 @@ class Parser(object):
 
     def __init__(self, query):
         self.query = query
+        self.query_id = None
+        self.initialize()
+
+    def initialize(self):
+        """
+        Perform Initial actions with query
+        """
+        self.save_query()
+
+    def save_query(self):
+        self.query_id = query_models.QueryStore.objects.create(text=self.query)
+
+    def keyword_processor(self):
+        keywords_with_weights = self.extract_keywords()
+        self.save_keywords(keywords_with_weights)
 
     def extract_keywords(self):
         """
@@ -40,3 +58,13 @@ class Parser(object):
             normalized_keywords_with_weight.append((x[0], x[1] / max_weight))
 
         return normalized_keywords_with_weight
+
+    def save_keywords(self, weighted_keywords):
+
+        obj_list = []
+
+        for kw, weight in weighted_keywords:
+            keyword, _ = keyword_models.Keywords.objects.get_or_create(keyword=kw)
+            obj_list.append(query_models.QueryKeywordStore(query=self.query_id, keyword=keyword, score=weight))
+
+        query_models.QueryKeywordStore.objects.bulk_create(obj_list)
