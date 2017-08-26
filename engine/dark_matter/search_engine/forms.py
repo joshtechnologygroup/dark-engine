37372b50e62df526e36d516b757f5338295000c3
Dhirendra <dhirendra.maurya@joshtechnologygroup.com>
2017-08-26 23:56:29 +0530

    updated ui

--- engine/dark_matter/search_engine/forms.py
+++ engine/dark_matter/search_engine/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(required=False, label='Search Query')
+    query = forms.CharField(required=False, label='Search Query', max_length=200)
