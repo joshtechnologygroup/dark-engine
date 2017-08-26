04d4a205e9891c1662149bf9ab9a11494c010374
Rohan Agarwal <rohan.agarwal@joshtechnologygroup.com>
2017-08-26 11:08:08 +0530

    Added feedback from past queries to rank current query keywords

--- engine/dark_matter/query_parser/parser.py
+++ engine/dark_matter/query_parser/parser.py
@@ -1,5 +1,7 @@
 import RAKE
-from nltk import corpus
+from nltk import (corpus, WordNetLemmatizer, Counter)
+
+from engine.dark_matter.query_parser.models import QueryStore, QueryKeywordStore
 
 
 class Parser(object):
@@ -23,9 +25,11 @@ class Parser(object):
         stopwords = corpus.stopwords.words('english')
         rake = RAKE.Rake(stopwords)
 
-        max_weight = 0
-        keywords_with_weight = rake.run(self.query)
+        query_terms = self.query.split(' ')
+        lemmatized_query = [WordNetLemmatizer().lemmatize(query_term) for query_term in query_terms]
+        keywords_with_weight = rake.run(' '.join(lemmatized_query))
 
+        max_weight = 0
         for x in keywords_with_weight:
             if x[1] > max_weight:
                 max_weight = x[1]
@@ -37,6 +41,45 @@ class Parser(object):
             return [[]]
 
         for x in keywords_with_weight:
-            normalized_keywords_with_weight.append((x[0], x[1] / max_weight))
+            normalized_keywords_with_weight.append([x[0], x[1] / max_weight])
+
+        for keyword in normalized_keywords_with_weight:
+            query_store = QueryStore(self.query)
+            query_store.save()
+            QueryKeywordStore(query=query_store, keyword=keyword[0], score=keyword[1]).save()
+        return normalized_keywords_with_weight
+
+    def process_past_queries(self, keywords_with_weight):
+        past_keywords_store = QueryKeywordStore.objects.filter(keyword__in=[keyword[0] for keyword in
+                                                                            keywords_with_weight])
+        score = {}
+        past_keywords_count = Counter(keyword.keyword for keyword in past_keywords_store)
+        for keyword in past_keywords_store:
+            if not score.get(keyword):
+                score[keyword] = keyword.score
+                past_keywords_store[keyword] = 1
+            else:
+                score[keyword] += keyword.score
+                past_keywords_store[keyword] += 1
+
+        for keyword in keywords_with_weight:
+            score[keyword] /= past_keywords_count.get(keyword)
+
+        for keyword in keywords_with_weight:
+            keyword[1] += score[keyword]
+
+        max_weight = 0
+        for x in keywords_with_weight:
+            if x[1] > max_weight:
+                max_weight = x[1]
+
+        normalized_keywords_with_weight = []
+
+        if not max_weight:
+            # No Data extracted
+            return [[]]
+
+        for x in keywords_with_weight:
+            normalized_keywords_with_weight.append([x[0], x[1] / max_weight])
 
         return normalized_keywords_with_weight
