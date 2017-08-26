b37796517087bc149530cbba0d5c95851111e091
Dhirendra <dhirendra.maurya@joshtechnologygroup.com>
2017-08-26 22:13:59 +0530

    UI

--- engine/dark_matter/commons/models.py
+++ engine/dark_matter/commons/models.py
@@ -2,30 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models import signals
-from django.db.models.query import QuerySet
-
-from dark_matter.commons import signals as commons_signals
-
-
-class SoftDeleteQuerySet(QuerySet):
-
-    def delete(self):
-        self.update(is_active=False)
-        for obj in self:
-            signals.post_delete.send(sender=obj.__class__, instance=obj)
-
-    def hard_delete(self):
-        return super(SoftDeleteQuerySet, self).delete()
-
-
-class ActiveObjectsManager(models.Manager):
-
-    def get_queryset(self):
-        return SoftDeleteQuerySet(self.model, using=self._db).filter(is_active=True)
-
-    def hard_delete(self):
-        return getattr(self.get_queryset(), "hard_delete")()
 
 
 class BaseModel(models.Model):
@@ -39,36 +15,5 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    objects = ActiveObjectsManager()
-    all_objects = models.Manager()
-
     class Meta:
         abstract = True
-
-    def delete(self, **kwargs):
-        """
-        Method to soft delete an object
-        """
-        # TODO delete related objects
-        signals.pre_delete.send(sender=self.__class__, instance=self)
-        super(BaseModel, self).delete(**kwargs)
-        self.is_active = False
-        self.save()
-        signals.post_delete.send(sender=self.__class__, instance=self)
-
-    def restore(self):
-        """
-        Method to restore soft deleted object.
-        If needed, add required signals
-        """
-
-        self.is_active = True
-        self.save()
-
-    def hard_delete(self, using=None):
-        super(BaseModel, self).delete(using)
-
-
-# Soft Delete other related objects in which referenced as Foreign Key or once-to-one field
-# TODO: This does not work well with hard-delete. Investigate and fix
-# signals.post_delete.connect(commons_signals.delete_related_objects)
--- engine/dark_matter/search_engine/views.py
+++ engine/dark_matter/search_engine/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import render
 
 
 # TODO: This is rough View
+from django.views import View
+from django.views.generic import ListView
 
 from dark_matter.query_parser import parser as query_parser
 from dark_matter.search_engine import ranker
 from dark_matter.entities.models import *
+from dark_matter.search_engine.forms import SearchForm
 
 
 def search_query(query_string):
@@ -18,3 +21,26 @@ def search_query(query_string):
     for row in results:
         print row['entity_text'].replace('\n', '').replace('\r', '')
         print row['entity_score']
+
+
+class ProfileList(View):
+    form_class = SearchForm
+    template_name = 'search.html'
+
+    def post(self, request, *args, **kwargs):
+
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            print form.data
+            qp = query_parser.Parser(form.cleaned_data['query'])
+            qp.keyword_processor()
+            results = ranker.Ranker(qp.query_object).processor()
+            response = []
+            for row in results:
+                response.append(row['entity_text'])
+
+            return render(request, self.template_name, {'results': response, 'form': form})
+        return render(request, self.template_name, {'form': self.form_class()})
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form_class()})
